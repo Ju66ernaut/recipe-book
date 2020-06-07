0852a9178ffbc11d06b6f1bf0f3ae2732dd6f909
Chris <ju66ernautdev@gmail.com>
2020-06-07 14:23:17 -0700

    updating sitename prop

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Recipe Book',
+  siteName: 'Diaz Family Recipe Book',
   siteDescription: 'SITE DESCRIPTION HERE',
 
   templates: {
